6fd854aa50d89103912126f6f34f1830223780a3
mychibani <m.yassinechibani@gmail.com>
2023-11-28 15:17:15 +0100

    R2 tells me that the model is totally wrong...

---
+++ prediction.py
@@ -0,0 +1,48 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+parser = argparse.ArgumentParser(
+	description="Predict a car's value using linear regression.")
+parser.add_argument('kms', help="car's mileage")
+parser.add_argument('--graph', '-g', action='store_true', help='show graph')
+options = parser.parse_args()
+
+# ------------------------------------------#
+
+
+def predict(theta0, theta1, input):
+	return theta0 + theta1 * input
+
+
+if __name__ == "__main__":
+	try:
+		thetas = pd.read_csv("thetas.csv")
+		theta0 = thetas.at[0, 'theta0']
+		theta1 = thetas.at[0, 'theta1']
+	except:
+		print("Error with the csv for thetas.")
+		exit()
+	if options.graph:
+		try:
+			data = pd.read_csv("data.csv")
+			X = data['km'].values
+			Y = data['price'].values
+			line = theta0 + theta1 * X
+		except:
+			print("Error with the csv file.")
+			exit(1)
+	predicted = predict(theta0, theta1, int(options.kms))
+	print("The price for a car with {} km is estimated at {}".format(
+		options.kms, round(predicted)))
+	if options.graph:
+            axes = plt.axes()
+            axes = plt.grid()
+            plt.scatter(data['km'].values, data['price'].values)
+            plt.plot(X, line, c='r')
+            plt.title('Linear Regression for price/mileage')
+            plt.xlabel('Mileage')
+            plt.ylabel('Price')
+            plt.show()
--- train.py
+++ train.py
@@ -1,53 +1,67 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
 # Load data from the CSV file (assuming it has columns "km" and "price")
 data = pd.read_csv('data.csv')
 
-X = data["km"].values
-Y = data["price"].values
-old_Y = Y
-old_X = X
-m = float(X.size)
-X = np.c_[np.ones(X.shape[0]), X]
-X = X / X.max()
-Y = Y / Y.max()
-learning_rate = 0.01
+
+def scaling_data(data, scale):
+	"""Scaling des data"""
+	return data / scale
 
 
-X = X * old_X.max()
+def csv_reader(path):
+    data = pd.read_csv(path)
+    X = np.array(data['km'].values)
+    old_X = X
+    X = scaling_data(X, max(X))
+    X = np.c_[np.ones(X.shape[0]), X]
+    Y = np.array(data['price'].values)  # OK
+    old_Y = Y
+    Y = scaling_data(Y, max(Y))
+    return X, old_X, Y, old_Y
 
 
-#def csv_reader(path):
-
-def h(thetas, x):
-    return (thetas[0] + thetas[1] * x)
-
-
-def gradient_descent(m, X, Y):
+def gradient_descent(m, X, Y, tmpthetas, learning_rate, epoch):
     tmpthetas = np.array([0, 0])
-    while (1):
-        oldthetas = tmpthetas;
+    for i in range(epoch):
+        oldthetas = tmpthetas
         tmpthetas = tmpthetas - learning_rate * (1 / m) * (X.T @ ((X @ tmpthetas) - Y))
         if np.array_equal(tmpthetas, oldthetas):
             break
     return tmpthetas
-            
-
-tmpthetas = gradient_descent(m, X, Y);
-tmpthetas[0] = tmpthetas[0] * max(old_Y)
-tmpthetas[1] = tmpthetas[1] * (max(old_Y) / max(old_Y))
-
-print(tmpthetas)
 
 
-# --> line = -0.02 * X + 8469 
+def calcul_thetas(X, Y, old_X, old_Y):
+    thetas = gradient_descent(float(len(X)), X, Y, np.array([0, 0]), 1, 100000)
+    thetas[0] = thetas[0] * max(old_Y)
+    thetas[1] = thetas[1] * (max(old_Y) / max(old_X))
+    return thetas
 
 
-plt.xlabel("Kilometrage")
-plt.ylabel("Price")
-plt.scatter(data['km'].values, data['price'].values)
-# plt.plot(X, line)
-plt.grid()
-plt.show()
+def throw_thethas_to_file(thethas):
+	"""Enregistrement des thetas dans le csv"""
+	with open("thetas.csv", "r+") as fd:
+		data = str(thethas[0]) + ',' + str(thethas[1]) + '\n'
+		newdata = ",theta0,theta1\n0," + data
+		fd.write(newdata)
+
+
+def accuracy(X, Y, thetas):
+    y_mean = Y.mean()
+    sumofsquares = 0
+    sumofresiduals = 0
+    for i in range (len(X)):
+         y_pred = thetas[0] + thetas[1] * X[i]
+         sumofsquares += (Y[i] - y_mean) ** 2
+         sumofresiduals += (Y[i] - y_pred) ** 2
+    score = 1 - (sumofsquares / sumofresiduals)
+    return (score)
+
+X, old_X, Y, old_Y = csv_reader("data.csv")
+thethas = calcul_thetas(X, Y, old_X, old_Y)
+throw_thethas_to_file(thethas)
+score = accuracy(old_X, old_Y, thethas)
+print(score)
